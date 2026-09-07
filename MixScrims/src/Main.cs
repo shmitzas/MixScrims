d6ebb05954dc8cb3a670e7244ff7f7ab5006d31c
shmitzas <46751666+shmitzas@users.noreply.github.com>
2026-09-07 14:52:10 +0300

    fix: Bumped version

--- MixScrims/src/Main.cs
+++ MixScrims/src/Main.cs
@@ -11,7 +11,7 @@ namespace MixScrims;
 
 [PluginMetadata(
     Id = "MixScrims",
-    Version = "1.11.1",
+    Version = "1.11.2",
     Name = "MixScrims",
     Author = "Shmitzas",
     Description = "A plugin for PUGS style matches, with in-game match management."
